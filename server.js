b977b0a78b1c84ef44d4e09a1c205c3adf590560
Joemer <joemers97@gmail.com>
2024-12-12 09:13:16 -0500

    Complete lab.

--- server.js
+++ server.js
@@ -6,44 +6,42 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
+const recipesController = require('./controllers/recipes.js');
+const ingredientsController = require('./controllers/ingredients.js');
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT ? process.env.PORT : '2000';
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
 );
+app.use(passUserToView);
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
-
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
+	res.render('index.ejs');
 });
 
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/recipes', recipesController);
+app.use('/ingredients', ingredientsController);
 
-app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+mongoose.connection.on('connected', () => {
+	console.clear();
+	console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+	app.listen(port, () => {
+		console.log(`The express app is ready on port ${port}!`);
+	});
 });
